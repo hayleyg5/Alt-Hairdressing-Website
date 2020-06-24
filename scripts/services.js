5411b363e29f5b64c836f95a8e89fd243ae92405
Hayley Goodman <31858311+hayleyg5@users.noreply.github.com>
2020-06-23 20:09:30 -0400

    Update services.js

--- scripts/services.js
+++ scripts/services.js
@@ -9,7 +9,7 @@ function validatePhone(txtPhone) {
     var a = document.getElementById(txtPhone).value;
     // This filter asks for something like (12345), so parentheses with any number (at least 1)
     // of digits
-    var filter = /^(\([-+]?[0-9]+)\)$/;
+    var filter = /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/;
     if (filter.test(a)) {
         return true;
     }
